9eca6a2addc5b83aa26eecd1fb86df9d4bcd4add
x0v0l <769338105@qq.com>
2023-10-20 17:32:01 +0800

    实现流式sniffer，持续监控wlan网卡

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -60,11 +62,34 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.capture_packets);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(656, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(567, 304);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(125, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 26);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "抓包停止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.stop_capture);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1296, 580);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -80,6 +105,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,14 +10,24 @@ using System.Windows.Forms;
 
 using SharpPcap;
 
+using System.Threading;
+
+
 
 namespace Sniffer_LLX
 {
     public partial class Form1 : Form
     {
+        private List<string> capturedPackets = new List<string>();
+        private int packetCount = 0;
+        private ICaptureDevice selectedDevice;
+        private bool capturing = false;
+        private Thread captureThread;
+
         public Form1()
         {
             InitializeComponent();
+            selectedDevice = GetSelectedDevice();
         }
 
         private void show_adapters(object sender, EventArgs e)
@@ -49,71 +59,186 @@ namespace Sniffer_LLX
 
         }
 
-        private List<string> capturedPackets = new List<string>();
-        private int packetCount = 0;
-
-        private void capture_packets(object sender, EventArgs e)
+        private ICaptureDevice GetSelectedDevice()
         {
-            // 获取网络适配器信息
             var devices = CaptureDeviceList.Instance;
-
-            // 找到指定网卡（Killer Wireless-n/a/ac 1435 Wireless Network Adapter）
-            ICaptureDevice selectedDevice = null;
             foreach (var dev in devices)
             {
                 if (dev.Description.Contains("Killer Wireless-n/a/ac 1435 Wireless Network Adapter"))
                 {
-                    selectedDevice = dev;
-                    break;
+                    return dev;
                 }
             }
+            return null;
+        }
 
-            if (selectedDevice == null)
+
+        private void capture_packets(object sender, EventArgs e)
+        {
+            //// 获取网络适配器信息
+            //var devices = CaptureDeviceList.Instance;
+
+            //// 找到指定网卡（Killer Wireless-n/a/ac 1435 Wireless Network Adapter）
+            //ICaptureDevice selectedDevice = null;
+            //foreach (var dev in devices)
+            //{
+            //    if (dev.Description.Contains("Killer Wireless-n/a/ac 1435 Wireless Network Adapter"))
+            //    {
+            //        selectedDevice = dev;
+            //        break;
+            //    }
+            //}
+
+            //if (selectedDevice == null)
+            //{
+            //    MessageBox.Show("未找到指定的网络适配器。");
+            //    return;
+            //}
+
+            //// 打开适配器以准备捕获数据包
+            //selectedDevice.OnPacketArrival += new PacketArrivalEventHandler(OnPacketArrival);
+            //selectedDevice.Open(DeviceMode.Promiscuous);
+            //selectedDevice.StartCapture();
+
+            //// 捕获10个数据包
+            //capturedPackets.Clear(); // 清空之前的捕获数据包
+            //packetCount = 0;
+
+            //while (packetCount < 10)
+            //{
+            //    // 暂停一段时间以等待数据包到达
+            //    System.Threading.Thread.Sleep(100);
+            //}
+
+            //// 停止捕获
+            //selectedDevice.StopCapture();
+            //selectedDevice.Close();
+
+            //// 显示捕获的数据包
+            //StringBuilder packetInfo = new StringBuilder();
+            //packetInfo.AppendLine("Captured Packets:");
+            //packetInfo.AppendLine("----------------------------------------------------");
+
+            //foreach (string packet in capturedPackets)
+            //{
+            //    packetInfo.AppendLine(packet);
+            //}
+
+            //// 显示捕获的数据包在弹窗中
+            //MessageBox.Show(packetInfo.ToString(), "Captured Packets");
+
+            if (selectedDevice != null)
+            {
+                if (!capturing)
+                {
+                    // 清空已捕获的数据包列表
+                    capturedPackets.Clear();
+                    capturing = true;
+
+                    // 创建一个新线程执行捕获
+                    captureThread = new Thread(CaptureThread);
+                    captureThread.Start();
+                }
+            }
+            else
             {
                 MessageBox.Show("未找到指定的网络适配器。");
-                return;
             }
 
-            // 打开适配器以准备捕获数据包
+        }
+
+        //private void OnPacketArrival(object sender, CaptureEventArgs e)
+        //{
+        //    // 处理捕获到的数据包
+        //    var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+        //    capturedPackets.Add(packet.ToString());
+        //    packetCount++;
+        //}
+
+        private void stop_capture(object sender, EventArgs e)
+        {
+            if (capturing)
+            {
+                capturing = false;
+                if (captureThread != null)
+                {
+                    captureThread.Join(); // 等待线程结束
+                }
+            }
+        }
+
+        private void CaptureThread()
+        {
             selectedDevice.OnPacketArrival += new PacketArrivalEventHandler(OnPacketArrival);
             selectedDevice.Open(DeviceMode.Promiscuous);
             selectedDevice.StartCapture();
 
-            // 捕获10个数据包
-            capturedPackets.Clear(); // 清空之前的捕获数据包
-            packetCount = 0;
-
-            while (packetCount < 10)
+            while (capturing)
             {
-                // 暂停一段时间以等待数据包到达
-                System.Threading.Thread.Sleep(100);
+                // 继续监听，不断捕获数据包
+                Thread.Sleep(100);
             }
 
             // 停止捕获
+            selectedDevice.OnPacketArrival -= new PacketArrivalEventHandler(OnPacketArrival);
             selectedDevice.StopCapture();
             selectedDevice.Close();
-
-            // 显示捕获的数据包
-            StringBuilder packetInfo = new StringBuilder();
-            packetInfo.AppendLine("Captured Packets:");
-            packetInfo.AppendLine("----------------------------------------------------");
-
-            foreach (string packet in capturedPackets)
-            {
-                packetInfo.AppendLine(packet);
-            }
-
-            // 显示捕获的数据包在弹窗中
-            MessageBox.Show(packetInfo.ToString(), "Captured Packets");
         }
 
         private void OnPacketArrival(object sender, CaptureEventArgs e)
         {
-            // 处理捕获到的数据包
-            var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
-            capturedPackets.Add(packet.ToString());
-            packetCount++;
+            if (capturing)
+            {
+                try
+                {
+                    // 处理捕获到的数据包
+                    var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+                    string packetInfo = packet.ToString();
+
+                    // 添加数据包到列表
+                    capturedPackets.Add(packetInfo);
+
+                    // 更新ListBox显示
+                    UpdatePacketListBox();
+
+                    // 限制捕获数据包的数量为1000
+                    if (capturedPackets.Count > 1000)
+                    {
+                        capturedPackets.RemoveAt(0); // 移除最旧的包
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // 记录异常信息
+                    capturedPackets.Add("Error: " + ex.Message);
+                    UpdatePacketListBox();
+                }
+            }
         }
 
+
+        private void UpdatePacketListBox()
+        {
+            // 更新ListBox显示
+            if (listBox1.InvokeRequired)
+            {
+                listBox1.BeginInvoke((MethodInvoker)delegate
+                {
+                    listBox1.BeginUpdate();
+                    listBox1.Items.Clear();
+                    listBox1.Items.AddRange(capturedPackets.ToArray());
+                    listBox1.EndUpdate();
+                    listBox1.TopIndex = listBox1.Items.Count - 1;
+                });
+            }
+            else
+            {
+                listBox1.BeginUpdate();
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(capturedPackets.ToArray());
+                listBox1.EndUpdate();
+                listBox1.TopIndex = listBox1.Items.Count - 1;
+            }
+        }
     }
 }
