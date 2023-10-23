fc926afa10f80e429dc5a76c54060806e859d219
x0v0l <769338105@qq.com>
2023-10-23 22:32:23 +0800

    实现了基础框架：捕获包信息为列表，点击某个包，会显示具体信息。分协议解析不同包的内容还有待完成

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,14 @@
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.macSource = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.macDestination = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ipSource = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ipDestination = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataLength = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -66,10 +74,10 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(656, 101);
+            this.listBox1.Location = new System.Drawing.Point(765, 101);
             this.listBox1.Name = "listBox1";
             this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(567, 304);
+            this.listBox1.Size = new System.Drawing.Size(519, 304);
             this.listBox1.TabIndex = 3;
             // 
             // button3
@@ -82,12 +90,88 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.stop_capture);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.macSource,
+            this.macDestination,
+            this.ipSource,
+            this.ipDestination,
+            this.dataLength});
+            this.dataGridView1.Location = new System.Drawing.Point(34, 161);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(725, 244);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // id
+            // 
+            this.id.FillWeight = 44.64286F;
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 50;
+            // 
+            // macSource
+            // 
+            this.macSource.FillWeight = 120.1765F;
+            this.macSource.HeaderText = "源MAC";
+            this.macSource.MinimumWidth = 6;
+            this.macSource.Name = "macSource";
+            this.macSource.ReadOnly = true;
+            this.macSource.Width = 135;
+            // 
+            // macDestination
+            // 
+            this.macDestination.FillWeight = 114.6958F;
+            this.macDestination.HeaderText = "目的MAC";
+            this.macDestination.MinimumWidth = 6;
+            this.macDestination.Name = "macDestination";
+            this.macDestination.ReadOnly = true;
+            this.macDestination.Width = 128;
+            // 
+            // ipSource
+            // 
+            this.ipSource.FillWeight = 110.2345F;
+            this.ipSource.HeaderText = "源IP";
+            this.ipSource.MinimumWidth = 6;
+            this.ipSource.Name = "ipSource";
+            this.ipSource.ReadOnly = true;
+            this.ipSource.Width = 124;
+            // 
+            // ipDestination
+            // 
+            this.ipDestination.FillWeight = 106.6031F;
+            this.ipDestination.HeaderText = "目的IP";
+            this.ipDestination.MinimumWidth = 6;
+            this.ipDestination.Name = "ipDestination";
+            this.ipDestination.ReadOnly = true;
+            this.ipDestination.Width = 119;
+            // 
+            // dataLength
+            // 
+            this.dataLength.FillWeight = 103.6472F;
+            this.dataLength.HeaderText = "数据段长度";
+            this.dataLength.MinimumWidth = 6;
+            this.dataLength.Name = "dataLength";
+            this.dataLength.ReadOnly = true;
+            this.dataLength.Width = 116;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1296, 580);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
@@ -95,6 +179,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,6 +192,13 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn macSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn macDestination;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ipSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ipDestination;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataLength;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ using SharpPcap;
 
 using System.Threading;
 
+using System.Collections.Concurrent;
+
 
 
 namespace Sniffer_LLX
@@ -19,6 +21,7 @@ namespace Sniffer_LLX
     public partial class Form1 : Form
     {
         private List<string> capturedPackets = new List<string>();
+        //private ConcurrentQueue<string> capturedPackets = new ConcurrentQueue<string>();
         private int packetCount = 0;
         private ICaptureDevice selectedDevice;
         private bool capturing = false;
@@ -129,16 +132,27 @@ namespace Sniffer_LLX
 
             if (selectedDevice != null)
             {
-                if (!capturing)
-                {
-                    // 清空已捕获的数据包列表
-                    capturedPackets.Clear();
-                    capturing = true;
+                //if (!capturing)
+                //{
+                //// 清空已捕获的数据包列表
+                //capturedPackets.Clear();
+                //capturing = true;
 
-                    // 创建一个新线程执行捕获
-                    captureThread = new Thread(CaptureThread);
-                    captureThread.Start();
-                }
+                //// 创建一个新线程执行捕获
+                //captureThread = new Thread(CaptureThread);
+                //captureThread.Start();
+
+                //// 清空已捕获的数据包列表
+                //capturedPackets = new ConcurrentQueue<string>();
+                //capturing = true;
+
+                //// 创建一个新线程执行捕获
+                //captureThread = new Thread(CaptureThread);
+                //captureThread.Start();
+                //}
+                selectedDevice.OnPacketArrival += new PacketArrivalEventHandler(OnPacketArrival);
+                selectedDevice.Open(DeviceMode.Promiscuous);
+                selectedDevice.StartCapture();
             }
             else
             {
@@ -157,13 +171,19 @@ namespace Sniffer_LLX
 
         private void stop_capture(object sender, EventArgs e)
         {
-            if (capturing)
+            //if (capturing)
+            //{
+            //    capturing = false;
+            //    if (captureThread != null)
+            //    {
+            //        captureThread.Join(); // 等待线程结束
+            //    }
+            //}
+            if (selectedDevice != null)
             {
-                capturing = false;
-                if (captureThread != null)
-                {
-                    captureThread.Join(); // 等待线程结束
-                }
+                selectedDevice.OnPacketArrival -= new PacketArrivalEventHandler(OnPacketArrival);
+                selectedDevice.StopCapture();
+                selectedDevice.Close();
             }
         }
 
@@ -187,31 +207,158 @@ namespace Sniffer_LLX
 
         private void OnPacketArrival(object sender, CaptureEventArgs e)
         {
-            if (capturing)
+            //if (capturing)
+            //{
+            //    try
+            //    {
+            //        // 处理捕获到的数据包
+            //        var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+            //        string packetInfo = packet.ToString();
+
+            //        // 添加数据包到列表
+            //        capturedPackets.Add(packetInfo);
+
+            //        // 更新ListBox显示
+            //        UpdatePacketListBox();
+
+            //        // 限制捕获数据包的数量为1000
+            //        if (capturedPackets.Count > 1000)
+            //        {
+            //            capturedPackets.RemoveAt(0); // 移除最旧的包
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        // 记录异常信息
+            //        capturedPackets.Add("Error: " + ex.Message);
+            //        UpdatePacketListBox();
+            //    }
+            //}
+
+            //if (capturing)
+            //{
+            //    try
+            //    {
+            //        // 处理捕获到的数据包
+            //        var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+
+            //        // 解析包信息，获取ID、MAC源地址、MAC目的地址、IP源地址、IP目的地址、数据字段长度
+            //        string id = capturedPackets.Count.ToString();
+            //        string macSource = ""; // 你需要编写解析代码来获取MAC源地址
+            //        string macDestination = ""; // 你需要编写解析代码来获取MAC目的地址
+            //        string ipSource = ""; // 你需要编写解析代码来获取IP源地址
+            //        string ipDestination = ""; // 你需要编写解析代码来获取IP目的地址
+            //        int dataLength = e.Packet.Data.Length;
+
+            //        //// 添加数据包到列表
+            //        //capturedPackets.Add($"{id}, {macSource}, {macDestination}, {ipSource}, {ipDestination}, {dataLength}");
+
+            //        // 添加数据包到队列
+            //        capturedPackets.Enqueue($"{id}, {macSource}, {macDestination}, {ipSource}, {ipDestination}, {dataLength}");
+
+            //        //// 更新DataGridView显示
+            //        //UpdatePacketDataGridView();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        //// 记录异常信息
+            //        //capturedPackets.Add("Error: " + ex.Message);
+            //        //UpdatePacketDataGridView();
+
+            //        // 记录异常信息
+            //        capturedPackets.Enqueue("Error: " + ex.Message);
+            //    }
+            //}
+            try
             {
-                try
+                // 处理捕获到的数据包
+                var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+
+                // 解析包信息，获取ID、MAC源地址、MAC目的地址、IP源地址、IP目的地址、数据字段长度
+                string id = capturedPackets.Count.ToString();
+                string macSource = ""; // 你需要编写解析代码来获取MAC源地址
+                string macDestination = ""; // 你需要编写解析代码来获取MAC目的地址
+                string ipSource = ""; // 你需要编写解析代码来获取IP源地址
+                string ipDestination = ""; // 你需要编写解析代码来获取IP目的地址
+                int dataLength = e.Packet.Data.Length;
+
+                // 添加数据包到列表
+                capturedPackets.Add($"{id}, {macSource}, {macDestination}, {ipSource}, {ipDestination}, {dataLength}");
+
+                // 更新DataGridView显示
+                UpdatePacketDataGridView();
+            }
+            catch (Exception ex)
+            {
+                // 记录异常信息
+                capturedPackets.Add("Error: " + ex.Message);
+                UpdatePacketDataGridView();
+            }
+        }
+
+        private void UpdatePacketDataGridView()
+        {
+            //if (dataGridView1.InvokeRequired)
+            //{
+            //    dataGridView1.BeginInvoke((MethodInvoker)delegate {
+            //        dataGridView1.Rows.Clear();
+            //        foreach (var packetInfo in capturedPackets)
+            //        {
+            //            string[] info = packetInfo.Split(',');
+            //            dataGridView1.Rows.Add(info);
+            //        }
+            //    });
+            //}
+            //else
+            //{
+            //    dataGridView1.Rows.Clear();
+            //    foreach (var packetInfo in capturedPackets)
+            //    {
+            //        string[] info = packetInfo.Split(',');
+            //        dataGridView1.Rows.Add(info);
+            //    }
+            //}
+
+            //dataGridView1.Rows.Clear();
+            //foreach (var packetInfo in capturedPackets)
+            //{
+            //    string[] info = packetInfo.Split(',');
+            //    dataGridView1.Rows.Add(info);
+            //}
+
+            if (capturedPackets.Count > 0)
+            {
+                string lastPacket = capturedPackets[capturedPackets.Count - 1];
+                string[] info = lastPacket.Split(',');
+                dataGridView1.Rows.Add(info);
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            //if (dataGridView1.SelectedRows.Count > 0)
+            //{
+            //    int selectedRow = dataGridView1.SelectedRows[0].Index;
+
+            //    if (selectedRow < capturedPackets.Count)
+            //    {
+            //        string packetInfo = capturedPackets.ToArray()[selectedRow];
+            //        // 显示包的详细信息在listBox1或其他控件中
+            //        listBox1.Items.Clear();
+            //        listBox1.Items.Add(packetInfo);
+            //    }
+            //}
+
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedRow = dataGridView1.SelectedRows[0].Index;
+
+                if (selectedRow < capturedPackets.Count)
                 {
-                    // 处理捕获到的数据包
-                    var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
-                    string packetInfo = packet.ToString();
-
-                    // 添加数据包到列表
-                    capturedPackets.Add(packetInfo);
-
-                    // 更新ListBox显示
-                    UpdatePacketListBox();
-
-                    // 限制捕获数据包的数量为1000
-                    if (capturedPackets.Count > 1000)
-                    {
-                        capturedPackets.RemoveAt(0); // 移除最旧的包
-                    }
-                }
-                catch (Exception ex)
-                {
-                    // 记录异常信息
-                    capturedPackets.Add("Error: " + ex.Message);
-                    UpdatePacketListBox();
+                    string packetInfo = capturedPackets[selectedRow];
+                    // 显示包的详细信息在listBox1或其他控件中
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(packetInfo);
                 }
             }
         }
@@ -240,5 +387,7 @@ namespace Sniffer_LLX
                 listBox1.TopIndex = listBox1.Items.Count - 1;
             }
         }
+        
+
     }
 }
